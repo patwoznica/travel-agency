f8a7e0fe2b3778d7af0ab304f0682efaf120658a
Patryk Woznica <pat.woznica@gmail.com>
2021-10-26 21:23:03 +0200

    Module 13.5

---
+++ src/redux/filtersRedux.js
@@ -0,0 +1,57 @@
+/* SELECTORS */
+
+export const getAllFilters = ({filters}) => filters;
+
+/* ACTIONS */
+
+// action name creator
+const reducerName = 'filters';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
+// TODO - add other action types
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
+// action creators
+export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
+// TODO - add other action creators
+export const changeDuration = (type, value) => ({ 
+  payload: {[type]: value}, 
+  type: CHANGE_DURATION });
+export const addTag = payload => ({payload, type: ADD_TAG});
+export const removeTag = payload => ({payload, type: REMOVE_TAG});
+// reducer
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    case CHANGE_PHRASE:
+      return {
+        ...statePart,
+        searchPhrase: action.payload,
+      };
+	  case CHANGE_DURATION:
+		  return {
+          ...statePart,
+          duration: {
+          ...statePart.duration,
+          ...action.payload,
+          },
+        }
+	  case ADD_TAG:
+		  return {
+			  ...statePart,
+			  tags: [...statePart.tags, action.payload],
+		  }
+	  case REMOVE_TAG:
+		  /*const tags = [...statePart.tags];
+		  const index = tags.indexOf(action.payload);
+		  tags.slice(index, 1);*/
+		  return {
+          ...statePart,
+          tags: statePart.tags.filter(tag => tag !== action.payload),
+        };
+    default:
+      return statePart;
+  }
+}
---
+++ src/redux/tripsRedux.js
@@ -0,0 +1,71 @@
+/* SELECTORS */
+
+export const getAllTrips = ({trips}) => trips;
+
+export const getFilteredTrips = ({trips, filters}) => {
+  let output = trips;
+
+  // filter by search phrase
+  if(filters.searchPhrase){
+    const pattern = new RegExp(filters.searchPhrase, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+  }
+
+  // TODO - filter by duration
+  if(filters.duration) {
+	  output = output.filter(
+	  (trip) => trip.days >= filters.duration.from && trip.days <= filters.duration.to); 
+  }
+  // TODO - filter by tags
+// if(filters.tags){
+//    for(let tag of filters.tags){
+//      output = output.filter(trip => trip.tags.includes(tag)); 
+//    }
+//  }
+	 if (filters.tags) {
+    output = output.filter(trip => filters.tags.every(tag => trip.tags.includes(tag)));
+  }
+  // TODO - sort by cost descending (most expensive goes first)
+  
+  return output;
+};
+
+export const getTripById = ({trips}, tripId) => {
+  const filtered = trips;
+
+  // TODO - filter trips by tripId
+
+  console.log('filtering trips by tripId:', tripId, filtered);
+  return filtered.length ? filtered[0] : {error: true};
+};
+
+export const getTripsForCountry = ({trips}, countryCode) => {
+  const filtered = trips;
+
+  // TODO - filter trips by countryCode
+
+  console.log('filtering trips by countryCode:', countryCode, filtered);
+  return filtered.length ? filtered : [{error: true}];
+};
+
+/* ACTIONS */
+
+/*
+// action name creator
+const reducerName = 'trips';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+
+
+// action creators
+
+
+// reducer
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    default:
+      return statePart;
+  }
+}
+ */
